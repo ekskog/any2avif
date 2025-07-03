52f2e98fe083f97be592e5c966664ca0dec4666e
Tayo Carvalhal <root@hbvu.su>
2025-07-03 12:35:37 +0200

    consolidate around port 3002

--- app.py
+++ app.py
@@ -451,7 +451,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=3000,
+        port=3002,
         log_level="info",
         access_log=True
     )
